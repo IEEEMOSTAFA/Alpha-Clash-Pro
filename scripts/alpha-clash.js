12857d6e0827b3b336ac01f2a577a1db75c31eb2
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-03-29 17:43:05 +0600

    GetRandom added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,11 +1,22 @@
+// function play(){
+//     // step - 1: hide the home screen . to hide the screen add the class hidden to the home section
+
+//        const homeSection = document.getElementById('home-screen')
+//        homeSection.classList.add('hidden');
+//     //    console.log(homeSection.classList);
+
+//     // show the playground 
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+//     // console.log(playgroundSection.classList);
+
+// }
+
+function continueGame(){
+    
+}
+
 function play(){
-    // step - 1: hide the home screen . to hide the screen add the class hidden to the home section
-
-       const homeSection = document.getElementById('home-screen')
-       homeSection.classList.add('hidden');
-    //    console.log(homeSection.classList);
-
-    // show the playground 
-    const playgroundSection = document.getElementById('play-ground');
-    console.log(playgroundSection.classList);
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
