16d26e7882dbe9976dd1f963bcebc4ac4449e732
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-03-31 13:59:23 +0600

    handlekeyboard added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,12 +11,37 @@
 //     // console.log(playgroundSection.classList);
 
 // }
+// function handleKeyboardButtonPress() {
+//     console.log('button pressed');
+// }
+// // capture keyboard key press 
+// document.addEventListener('keyup',handleKeyboardButtonPress)
 
-function continueGame(){
-    
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+     
+    // key player is expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+
+    // check right or wrong key pressed:
+    if(playerPressed === expectedAlphabet){
+        console.log('You got a point');
+    }
+    else{
+        console.log('You are fail');
+    }
+   
+}
+document.addEventListener('keyup',handleKeyboardKeyUpEvent);
+
+function continueGame() {
+
     // step -1::  generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet',alphabet);
+    console.log('your random alphabet', alphabet);
 
     // set randomly generated alphabet to the screen (show it);
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -24,11 +49,11 @@ function continueGame(){
 
     // set background color
     setBackgroundColorById(alphabet);
-     
+
 }
 
 
-function play(){
+function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
     continueGame();
